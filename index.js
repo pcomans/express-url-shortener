beb70f2da7642968ae93ac897e82a3d2ff6c4535
Philipp Comans <philipp.comans@gmail.com>
2018-04-23 17:19:19 -0700

    Store keys in local cache until I figure out how to use PG in express

--- index.js
+++ index.js
@@ -4,29 +4,43 @@ const base32 = require("./base32");
 const express = require("express");
 const app = express();
 
+const cache = new Map();
+
 app.get("/", function(req, res) {
   res.send("Web interface goes here");
 });
 
 app.get("/:shortUrl", function(req, res) {
-  console.log(`Should return long URL for ${req.params.shortUrl}`);
-  res.send(req.params);
+  let shortUrl = req.params.shortUrl.trim();
+  if (!cache.has(shortUrl)) {
+    res.send("NOT FOUND");
+  } else {
+    res.redirect(cache.get(shortUrl));
+  }
 });
 
 app.post(
   "/s/:longUrl",
-  function(req, res, next) {
-    console.log(`Should shorten long URL ${req.params.longUrl}`);
-    
-    let key= base32.get8();
-    console.log(`The key is: ${key}`);
-
-    next();
-  },
   function(req, res) {
-    console.log("Should return shortened URL");
-    res.send(req.params);
+    let key = base32.get8();
+    console.log(`The key is: ${key}`);
+    cache.set(key, req.params.longUrl);
+
+    res.send({
+      longUrl: req.params.longUrl,
+      shortUrl: key,
+    });
   }
 );
+
+const config = {
+  user: "maxroach",
+  host: "localhost",
+  database: "shorturl",
+  port: 26257
+};
+
+// Create a pool.
+const pool = new pg.Pool(config);
 
 app.listen(3000, () => console.log("App listening on port 3000!"));
